c715040c31c5abb6814e88eef7c481673f4a5c07
vi3318 <vihaan.dharia039@nmims.edu.in>
2025-07-17 01:39:13 +0530

    port1

--- app.py
+++ app.py
@@ -1128,5 +1128,4 @@ def handle_file_share_message_event(event):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0")
